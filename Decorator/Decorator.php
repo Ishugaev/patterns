d1003ba1d97a1ff7b0fb7d0fd0615ae027a51902
Ishugaev <ishugaev.developer@gmail.com>
2016-03-03 17:40:15 +0200

    Update Decorator.php

--- Decorator/Decorator.php
+++ Decorator/Decorator.php
@@ -1,9 +1,24 @@
 <?php
 
+class RequestHelper 
+{
+  
+}
+
+abstract class ProcessRequest 
+{
+  abstract function process(RequestHelper $request);
+}
+
 /**
  * Class Decorator
  */
-class Decorator
+abstract class ProcessDecorator extends ProcessRequest
 {
-
+  protected $processRequest;
+  
+  public function __construct(ProcessRequest $processRequest)
+  {
+    $this->processRequest = $processRequest;
+  }
 }
