efb553f75b377d7baedee06c19d96a4a80a86178
Igor Shugaev <ishugaev.developer@gmail.com>
2016-03-06 11:51:24 +0200

    Improved Decorator draft implementation

--- Decorator/Decorator.php
+++ Decorator/Decorator.php
@@ -1,24 +1,50 @@
 <?php
 
-class RequestHelper 
-{
-  
-}
+class RequestHelper {}
 
 abstract class ProcessRequest 
 {
-  abstract function process(RequestHelper $request);
+    abstract public function process(RequestHelper $request);
+}
+
+class MainProcess extends ProcessRequest
+{
+    public function process(RequestHelper $request) {
+        print __CLASS__ . ' @TODO: Implement process request functionality';
+    }
 }
 
 /**
+ * The main idea of Decorator pattern is that we store object in some property inside class Decorator
+ * and then applying custom Decorator logic; instead of just using inheritance
+ *
  * Class Decorator
  */
 abstract class ProcessDecorator extends ProcessRequest
 {
-  protected $processRequest;
-  
-  public function __construct(ProcessRequest $processRequest)
-  {
-    $this->processRequest = $processRequest;
-  }
+    protected $processRequest;
+
+    public function __construct(ProcessRequest $processRequest) {
+        $this->processRequest = $processRequest;
+    }
 }
+
+class LogRequest extends ProcessDecorator
+{
+    public function process(RequestHelper $request) {
+        $this->processRequest->process();
+        print __CLASS__ .' @TODO: Add logging functionality';
+    }
+}
+
+class AuthenticateRequest extends ProcessDecorator
+{
+    public function process(RequestHelper $request)
+    {
+        $this->processRequest->process();
+        print __CLASS__ . ' @TODO: Add authentication functionality';
+    }
+}
+
+$process = new AuthenticateRequest(new LogRequest(new MainProcess()));
+$process->process(new RequestHelper());
